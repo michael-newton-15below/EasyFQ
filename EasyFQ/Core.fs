50404812fbe2af71d998c2d09c3cb68c1c3dc3e4
MJN <michael.newton@15below.com>
2012-11-15 14:12:10 +0000

    Initial commit.

---
+++ EasyFQ/Core.fs
@@ -0,0 +1,16 @@
+﻿module EasyFQ
+
+open System
+open EasyNetQ
+open FSharp.Control
+
+let inline SeqSubscribe<'T> (bus : IBus) subscriptionId =
+    let bq = new BlockingQueueAgent<'T>(1)
+
+    let rec messageSeq () = seq {
+        yield bq.Get()
+        yield! messageSeq()
+    }
+
+    bus.Subscribe(subscriptionId, fun message -> bq.Add message)
+    messageSeq ()
---
+++ EasyFQ/Script.fsx
@@ -0,0 +1,42 @@
+﻿// This file is a script that can be executed with the F# Interactive.  
+// It can be used to explore and test the library project.
+// Note that script files will not be part of the project build.
+
+#I "bin/release"
+#I "bin/debug"
+#r "EasyNetQ"
+#r "FSharpx.Core"
+#load "Core.fs"
+open EasyNetQ
+open EasyFQ
+
+type MyMessage = { message : string }
+
+let bus = RabbitHutch.CreateBus "host=localhost"
+
+let messages = SeqSubscribe<MyMessage> bus "hello_world"
+
+let tokenSource = new System.Threading.CancellationTokenSource()
+
+let agent = MailboxProcessor<unit>.Start(fun inbox -> async {
+        let rec inner () =
+            let message = Seq.head messages
+            printfn "%s" message.message
+            inner ()
+        inner()
+    }, tokenSource.Token)
+
+let publishMessage message =
+    use pub = bus.OpenPublishChannel ()
+    pub.Publish ({message = message })
+
+let startTime = System.DateTime.Now
+
+for i in [1..100000] do
+    publishMessage (i.ToString())
+
+tokenSource.Cancel ()
+
+let endTime = System.DateTime.Now
+
+printfn "%A" (endTime - startTime)
